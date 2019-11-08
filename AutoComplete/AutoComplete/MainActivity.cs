8f0533cc5975013a711775b0eb686414305a7467
nikola <nikolavodeni4arov@gmail.com>
2019-11-08 14:36:23 +0200

    Exercising autocomplete.

---
+++ AutoComplete/AutoComplete/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace AutoComplete
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.main);
+
+            var countries = Resources.GetStringArray(Resource.Array.countries);
+            var countryAutocomplete = FindViewById<AutoCompleteTextView>(Resource.Id.countryAutocomplete);
+            var adapter = new ArrayAdapter<string>(this, Resource.Layout.list_item, countries);
+
+            countryAutocomplete.Adapter = adapter;
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
