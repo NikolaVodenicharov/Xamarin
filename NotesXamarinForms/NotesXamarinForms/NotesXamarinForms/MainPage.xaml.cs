3256e4d9c1e0bc8960a0b1eeef6bd13c19d9a16a
nikola <nikolavodeni4arov@gmail.com>
2019-11-14 11:55:42 +0200

    Saving note to txt file with Xamarin.Forms.

---
+++ NotesXamarinForms/NotesXamarinForms/NotesXamarinForms/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NotesXamarinForms
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private readonly string fileName = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), 
+                "notes.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(this.fileName))
+            {
+                editor.Text = File.ReadAllText(this.fileName);
+            }
+        }
+
+        private void OnSaveButtonClick(object sender, EventArgs args)
+        {
+            File.WriteAllText(this.fileName, editor.Text);
+        }
+
+        private void OnDeleteButtonClick(object sender, EventArgs args)
+        {
+            if (File.Exists(this.fileName))
+            {
+                File.Delete(this.fileName);
+            }
+
+            editor.Text = string.Empty;
+        }
+    }
+}
