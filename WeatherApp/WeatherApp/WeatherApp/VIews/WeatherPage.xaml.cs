7c9c2ce1641f374df07b041a434fd4f3fcf2f3d9
nikola <nikolavodeni4arov@gmail.com>
2019-12-12 01:04:52 +0200

    Start implementing ModelView for weather.

--- WeatherApp/WeatherApp/WeatherApp/VIews/WeatherPage.xaml.cs
+++ WeatherApp/WeatherApp/WeatherApp/VIews/WeatherPage.xaml.cs
@@ -12,80 +12,80 @@ namespace WeatherApp.VIews
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WeatherPage : ContentPage
     {
-        private IWeatherRepository repository;
+        //private IWeatherRepository repository;
 
         public WeatherPage()
         {
             InitializeComponent();
 
-            this.repository = new OpenWeatherApiRepository();
+            //this.repository = new OpenWeatherApiRepository();
 
-            this.searchEntry.TextChanged += this.OnSearchEntryTextChanged;
-            this.suggestionsList.ItemTapped += this.OnSuggestionListItemtapped;
+            //this.searchEntry.TextChanged += this.OnSearchEntryTextChanged;
+            //this.suggestionsList.ItemTapped += this.OnSuggestionListItemtapped;
         }
 
-        private async void OnGetWeatherButtonClicked(object sender, EventArgs args)
-        {
-            var keyword = searchEntry.Text;
+        //private async void OnGetWeatherButtonClicked(object sender, EventArgs args)
+        //{
+        //    var keyword = searchEntry.Text;
 
-            if (String.IsNullOrEmpty(keyword))
-            {
-                return;
-            }
+        //    if (String.IsNullOrEmpty(keyword))
+        //    {
+        //        return;
+        //    }
 
-            var isAutocompleteKeyword = keyword.Contains('(');
-            if (isAutocompleteKeyword)
-            {
-                var pattern = @"[A-Za-z \-]+(?=[(])";
-                keyword = Regex.Match(keyword, pattern).Value;
-            }
+        //    var isAutocompleteKeyword = keyword.Contains('(');
+        //    if (isAutocompleteKeyword)
+        //    {
+        //        var pattern = @"[A-Za-z \-]+(?=[(])";
+        //        keyword = Regex.Match(keyword, pattern).Value;
+        //    }
 
 
-            var weather = await repository.ReadByCityNameAsync(keyword);
-            if (weather == null)
-            {
-                this.CityNotFoundAlert(keyword);
-            }
-            else
-            {
-                this.BindingContext = weather;
-            }
+        //    var weather = await repository.ReadByCityNameAsync(keyword);
+        //    if (weather == null)
+        //    {
+        //        this.CityNotFoundAlert(keyword);
+        //    }
+        //    else
+        //    {
+        //        this.BindingContext = weather;
+        //    }
 
-            this.searchEntry.Text = string.Empty;
-            this.searchEntry.Placeholder = "Enter city";
-        }
-        private async void CityNotFoundAlert(string cityName)
-        {
-            await DisplayAlert(
-                $"{cityName} was not found", 
-                "The city that you are searching is not found. Make sure that you type is correct.", 
-                "OK");
-        }
+        //    this.searchEntry.Text = string.Empty;
+        //    this.searchEntry.Placeholder = "Enter city";
+        //}
+        //private async void CityNotFoundAlert(string cityName)
+        //{
+        //    await DisplayAlert(
+        //        $"{cityName} was not found", 
+        //        "The city that you are searching is not found. Make sure that you type is correct.", 
+        //        "OK");
+        //}
 
-        private void OnSearchEntryTextChanged(object sender, EventArgs args)
-        {
-            var keyword = this.searchEntry.Text.ToLower();
-            if (String.IsNullOrEmpty(keyword))
-            {
-                return;
-            }
+        //private void OnSearchEntryTextChanged(object sender, EventArgs args)
+        //{
+        //    var keyword = this.searchEntry.Text.ToLower();
+        //    if (String.IsNullOrEmpty(keyword))
+        //    {
+        //        return;
+        //    }
 
-            var suggestions = App
-                .cities.Where(c => c.ToLower().StartsWith(keyword))
-                .Take(5)
-                .ToList();
+        //    var suggestions = App
+        //        .cities.Where(c => c.ToLower().StartsWith(keyword))
+        //        .Take(5)
+        //        .ToList();
 
-            this.suggestionsList.ItemsSource = suggestions;
-            this.suggestionsList.IsVisible = true;
-        }
+        //    this.suggestionsList.ItemsSource = suggestions;
+        //    this.suggestionsList.IsVisible = true;
+        //}
 
-        private void OnSuggestionListItemtapped(object sender, ItemTappedEventArgs e)
-        {
-            var city = (string)e.Item;
-            this.searchEntry.Text = city;
+        //private void OnSuggestionListItemtapped(object sender, ItemTappedEventArgs e)
+        //{
+        //    var city = (string)e.Item;
+        //    this.searchEntry.Text = city;
 
-            this.suggestionsList.IsVisible = false;
-            this.suggestionsList.ItemsSource = null;
-        }
+        //    this.suggestionsList.IsVisible = false;
+        //    this.suggestionsList.ItemsSource = null;
+        //}
     }
 }
--- WeatherApp/WeatherApp/WeatherApp/VeiwModels/WeatherViewModel.cs
+++ WeatherApp/WeatherApp/WeatherApp/VeiwModels/WeatherViewModel.cs
@@ -1,10 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using WeatherApp.Core.Models;
+using WeatherApp.Core.Repositories;
+using WeatherApp.Data.Repositories;
+using Xamarin.Forms;
 
 namespace WeatherApp.VeiwModels
 {
-    public class WeatherViewModel
+    public class WeatherViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private IWeatherRepository repository;
+
+        public WeatherViewModel()
+        {
+            repository = new OpenWeatherApiRepository();
+            GetWeatherCommand = new Command<string>(GetWeather);
+        }
+
+        public string Title { get; set; } = String.Empty;
+        public string Temperature { get; set; } = String.Empty;
+        public string Wind { get; set; } = String.Empty;
+        public string Humidity { get; set; } = String.Empty;
+        public string Visibility { get; set; } = String.Empty;
+        public string Sunrise { get; set; } = String.Empty;
+        public string Sunset { get; set; } = String.Empty;
+
+        public ICommand GetWeatherCommand { get; private set; }
+
+        private async void GetWeather(string searchEntry)
+        {
+            if (string.IsNullOrEmpty(searchEntry))
+            {
+                return;
+            }
+
+            var isAutocompleteKeyword = searchEntry.Contains('(');
+            if (isAutocompleteKeyword)
+            {
+                var pattern = @"[A-Za-z \-]+(?=[(])";
+                searchEntry = Regex.Match((string)searchEntry, pattern).Value;
+            }
+
+            var weather = await repository.ReadByCityNameAsync((string)searchEntry);
+            if (weather == null)
+            {
+                //this.CityNotFoundAlert(keyword);
+            }
+            else
+            {
+                SetWeatherProperties(weather);
+            }
+
+            //this.searchEntry.Text = string.Empty;
+            //this.searchEntry.Placeholder = "Enter city";
+        }
+        private void SetWeatherProperties(Weather weather)
+        {
+            this.Title = weather.Title;
+            OnPropertyChanged(nameof(Title));
+
+            this.Temperature = weather.Temperature;
+            OnPropertyChanged(nameof(Temperature));
+
+            this.Wind = weather.Wind;
+            OnPropertyChanged(nameof(Wind));
+
+            this.Humidity = weather.Humidity;
+            OnPropertyChanged(nameof(Humidity));
+
+            this.Visibility = weather.Visibility;
+            OnPropertyChanged(nameof(Visibility));
+
+            this.Sunrise = weather.Sunrise;
+            OnPropertyChanged(nameof(Sunrise));
+
+            this.Sunset = weather.Sunset;
+            OnPropertyChanged(nameof(Sunset));
+        }
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        //private async void CityNotFoundAlert(string cityName)
+        //{
+        //    await DisplayAlert(
+        //        $"{cityName} was not found",
+        //        "The city that you are searching is not found. Make sure that you type is correct.",
+        //        "OK");
+        //}
+
+        //private void OnSearchEntryTextChanged(object sender, EventArgs args)
+        //{
+        //    var keyword = this.searchEntry.Text.ToLower();
+        //    if (String.IsNullOrEmpty(keyword))
+        //    {
+        //        return;
+        //    }
+
+        //    var suggestions = App
+        //        .cities.Where(c => c.ToLower().StartsWith(keyword))
+        //        .Take(5)
+        //        .ToList();
+
+        //    this.suggestionsList.ItemsSource = suggestions;
+        //    this.suggestionsList.IsVisible = true;
+        //}
+
+        //private void OnSuggestionListItemtapped(object sender, ItemTappedEventArgs e)
+        //{
+        //    var city = (string)e.Item;
+        //    this.searchEntry.Text = city;
+
+        //    this.suggestionsList.IsVisible = false;
+        //    this.suggestionsList.ItemsSource = null;
+        //}
     }
 }
